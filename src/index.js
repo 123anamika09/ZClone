23b02a7fb13fb12084e8f2f1b27b15387fd69f3a
Anamika <anamika120502@gmail.com>
2025-08-12 21:58:46 +0530

    Dashboard added

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import './index.css';
+import Home from "./components/Home";
+import Orders from "./components/Orders";
+import Holdings from "./components/Holdings";
+import Positions from "./components/Positions";
+import Funds from "./components/Funds";
+import Apps from "./components/Apps";
 
-import HomePage from "./landing_page/home/HomePage";
-import Signup from "./landing_page/signup/Signup";
-import AboutPage from "./landing_page/about/AboutPage";
-import ProductPage from "./landing_page/products/ProductsPage";
-import PricingPage from "./landing_page/pricing/PricingPage";
-import SupportPage from "./landing_page/support/SupportPage";
 
-import NotFound from "./landing_page/NotFound";
-import Navbar from "./landing_page/Navbar";
-import Footer from "./landing_page/Footer";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Navbar />
+  <React.StrictMode>
+    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/product" element={<ProductPage />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/orders" element={<Home/>} />
+      <Route path="/holdings" element={<Home/>} />
+      <Route path="/positions" element={<Home/>} />
+      <Route path="/funds" element={<Home/>} />
+      <Route path="/apps" element={<Home/>} />
     </Routes>
-    <Footer />
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
